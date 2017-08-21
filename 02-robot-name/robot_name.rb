d9105e540c95d2d5251152601638d6442907ff75
Tanja Stroble <plusdeltah@gmail.com>
2017-08-21 16:11:23 -0700

    completed

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,29 @@
+# NAME_REGEXP = /^[A-Z]{2}\d{3}$/
+require 'faker'
 
+class Robot
+  attr_accessor :robot, :name
+
+def initialize
+  rand_name = Faker::Base.regexify(/^[A-Z]{2}\d{3}$/)
+  name = rand_name
+  @name = name
+end
+
+def reset
+  rand_name = Faker::Base.regexify(/^[A-Z]{2}\d{3}$/)
+  while rand_name == name
+    rand_name = Faker::Base.regexify(/^[A-Z]{2}\d{3}$/)
+  end
+name2 = rand_name
+@name = name2
+return @name
+
+end
+
+end
+
+Robot.new.name
+
+robot = Robot.new
+name  = robot.name
