60cbe7bfefda8b125be611716c66cc0d3a1f0730
Tanja Stroble <plusdeltah@gmail.com>
2017-08-26 21:31:20 -0700

    uncommented skips

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,12 @@
 
+
+
+def words(sentence)
+sentence = sentence.upcase.split(" ")
+hash = {}
+sentence.uniq.each{|e| hash[e]= sentence.count(e)}
+puts hash
+end
+
+
+#words("You are the Sun and when the sun is gone, you are the moon!")
